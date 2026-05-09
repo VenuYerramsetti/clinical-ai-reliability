6fabfdccab7fd1edda8f7afdab07a26fa69f5665
Venu Madhuri Yerramsetti <your_email@gmail.com>
2026-05-09 16:38:41 +0530

    Added training time tracking

--- src/train.py
+++ src/train.py
@@ -1,4 +1,3 @@
-
 # ====================================
 # CLINICAL AI RELIABILITY PROJECT
 # TRAINING PIPELINE
@@ -8,6 +7,8 @@
 # ====================================
 # IMPORTS
 # ====================================
+
+import time
 
 import torch
 import torch.nn as nn
@@ -23,6 +24,7 @@ from data_loader import (
     label_mapping,
     device
 )
+
 
 # ====================================
 # LOAD PRETRAINED RESNET18
@@ -51,7 +53,8 @@ model.fc = nn.Linear(
 
 model = model.to(device)
 
-print("\nModel initialized successfully! ")
+print("\nModel initialized successfully!")
+
 
 # ====================================
 # LOSS FUNCTION
@@ -60,6 +63,7 @@ print("\nModel initialized successfully! ")
 criterion = nn.CrossEntropyLoss()
 
 print("\nLoss function created!")
+
 
 # ====================================
 # OPTIMIZER
@@ -71,3 +75,96 @@ optimizer = optim.Adam(
 )
 
 print("\nOptimizer initialized!")
+
+
+# ====================================
+# TRAINING CONFIG
+# ====================================
+
+NUM_EPOCHS = 3
+
+print("\nTraining configuration ready!")
+
+
+# ====================================
+# START TRAINING TIMER
+# ====================================
+
+training_start_time = time.time()
+
+
+# ====================================
+# TRAINING LOOP
+# ====================================
+
+for epoch in range(NUM_EPOCHS):
+
+    # Start epoch timer
+    epoch_start_time = time.time()
+
+    print(f"\nEpoch {epoch + 1}/{NUM_EPOCHS}")
+
+    # Set model to training mode
+    model.train()
+
+    running_loss = 0.0
+
+    # Loop through batches
+    for images, labels in train_loader:
+
+        # Move tensors to GPU/CPU
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Clear previous gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(images)
+
+        # Compute loss
+        loss = criterion(outputs, labels)
+
+        # Backpropagation
+        loss.backward()
+
+        # Update weights
+        optimizer.step()
+
+        # Track total loss
+        running_loss += loss.item()
+
+    # Average epoch loss
+    epoch_loss = running_loss / len(train_loader)
+
+    print(f"Training Loss: {epoch_loss:.4f}")
+
+    # End epoch timer
+    epoch_end_time = time.time()
+
+    epoch_duration = (
+        epoch_end_time - epoch_start_time
+    )
+
+    print(
+        f"Epoch Time: "
+        f"{epoch_duration:.2f} seconds"
+    )
+
+
+# ====================================
+# TOTAL TRAINING TIME
+# ====================================
+
+training_end_time = time.time()
+
+total_training_time = (
+    training_end_time - training_start_time
+)
+
+print("\nTraining completed!")
+
+print(
+    f"Total Training Time: "
+    f"{total_training_time:.2f} seconds"
+)
