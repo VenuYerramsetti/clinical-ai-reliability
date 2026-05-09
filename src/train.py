ca90672ddb14a3c52fe64f643b991b8690474b84
Venu Madhuri Yerramsetti <your_email@gmail.com>
2026-05-09 15:56:34 +0530

    Created training pipeline

---
+++ src/train.py
@@ -0,0 +1,54 @@
+
+# ====================================
+# CLINICAL AI RELIABILITY PROJECT
+# TRAINING PIPELINE
+# ====================================
+
+
+# ====================================
+# IMPORTS
+# ====================================
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchvision import models
+
+# Import objects from data_loader.py
+from data_loader import (
+    train_loader,
+    val_loader,
+    test_loader,
+    label_mapping,
+    device
+)
+
+# ====================================
+# LOAD PRETRAINED RESNET18
+# ====================================
+
+weights = models.ResNet18_Weights.DEFAULT
+
+model = models.resnet18(weights=weights)
+
+
+# ====================================
+# MODIFY FINAL LAYER
+# ====================================
+
+num_classes = len(label_mapping)
+
+model.fc = nn.Linear(
+    model.fc.in_features,
+    num_classes
+)
+
+
+# ====================================
+# MOVE MODEL TO DEVICE
+# ====================================
+
+model = model.to(device)
+
+print("\nModel initialized successfully!")
